0cbcf11f7638f6fc4cfc85dcc578899daa2247fa
Painezor <Paine.exe@gmail.com>
2017-10-30 21:30:59 +0000

    fixes

--- ext/mtb.py
+++ ext/mtb.py
@@ -22,6 +22,7 @@ class MatchThreads:
 		self.postat = ""
 		self.scheduler = True
 		self.schedtask = self.bot.loop.create_task(self.mt_scheduler())
+		self.headerline = ""
 	
 	def __unload(self):
 		self.schedtask.cancel()
@@ -65,10 +66,17 @@ class MatchThreads:
 				await asyncio.sleep(sleepuntil) # Sleep bot until then.
 				await self.bot.loop.create_task(self.automt())
 				await asyncio.sleep(180)
-				
+	
+	@commands.command()
+	@commands.is_owner()
+	async def forcemt(self,ctx):
+		await self.bot.loop.create_task(self.automt())
+		await ctx.send("Shit. Ok. Posting.")
+	
 	async def automt(self):
 		""" This is the bit that does the match thread. """
 		modch = self.bot.get_channel(306552425144385536)
+		print("Nar I'm running!")
 		async with self.bot.session.get("http://www.bbc.co.uk/sport/football/teams/newcastle-united/scores-fixtures") as resp:
 			if resp.status != 200:
 				await modch.send(":no_entry_sign: Match Thread Bot Aborted: HTTP Error: attempting to access game listings. {resp.url} returned status {resp.status}")
@@ -103,9 +111,9 @@ class MatchThreads:
 				ko = f'{ko}\n\n'
 				try:
 					kotime = tree.xpath('//span[@class="fixture__number fixture__number--time"]/text()')[0]
-					ko = f"[🕒](#icon-clock) **Kickoff**: {kotime} on {ko}"
+					ko = f"[ðŸ•’](#icon-clock) **Kickoff**: {kotime} on {ko}"
 				except IndexError:
-					ko = f"[🕒](#icon-clock) **Kickoff**: {self.ko}"
+					ko = f"[ðŸ•’](#icon-clock) **Kickoff**: {self.ko}"
 				print(f"A: {ko}")
 				home = tree.xpath('//span[@class="fixture__team-name fixture__team-name--home"]//abbr/@title')[0]
 				away = tree.xpath('//span[@class="fixture__team-name fixture__team-name--away"]//abbr/@title')[0]
@@ -115,9 +123,9 @@ class MatchThreads:
 				ko = f'{ko}\n\n'
 				try:
 					kotime = tree.xpath('//span[@class="fixture__number fixture__number--time"]/text()')[0]
-					ko = f"[🕒](#icon-clock) **Kickoff**: {kotime} on {ko}"
+					ko = f"[ðŸ•’](#icon-clock) **Kickoff**: {kotime} on {ko}"
 				except IndexError:
-					ko = f"[🕒](#icon-clock) **Kickoff**: {ko}"
+					ko = f"[ðŸ•’](#icon-clock) **Kickoff**: {ko}"
 				teams = tree.xpath('//div[@class="fixture__wrapper"]//abbr/@title')
 				home = teams[0]
 				away = teams[1]
@@ -136,17 +144,17 @@ class MatchThreads:
 							if away in "".join(i.xpath('.//td[5]//text()')).strip():
 								fnd = i.xpath('.//td[6]//a/@href')[-1]
 								fnd = f"http://www.livesoccertv.com/{fnd}"
-								tv = f"[📺](#icon-tv)[Television Coverage]({fnd})\n\n"
+								tv = f"[ðŸ“º](#icon-tv)[Television Coverage]({fnd})\n\n"
 			hreddit = self.bot.teams[home]['subreddit']
 			hicon = self.bot.teams[home]['icon']
 			areddit = self.bot.teams[away]['subreddit']
 			aicon = self.bot.teams[away]['icon']
 			venue = f"[{self.bot.teams[home]['stadium']}]({self.bot.teams[home]['stadlink']})"
-			ground = f"[🥅](#icon-net) **Venue**: {venue}\n\n"
+			ground = f"[ðŸ¥…](#icon-net) **Venue**: {venue}\n\n"
 			archive = "[Match Thread Archive](https://www.reddit.com/r/NUFC/wiki/archive)\n\n"
 			try:
 				ref  = tree.xpath('//dd[@class="description-list__description"]/text()[1]')[0]
-				ref = f"[ℹ](#icon-whistle) **Referee**: {ref}\n\n"
+				ref = f"[â„¹](#icon-whistle) **Referee**: {ref}\n\n"
 			except IndexError:
 				ref = ""
 	
@@ -164,11 +172,11 @@ class MatchThreads:
 			awayx = tree.xpath('.//ul[@class="gs-o-list-ui gs-o-list-ui--top-no-border gel-pica"][1]/li')[11:]
 			hgoals = "".join(tree.xpath('.//ul[contains(@class,"fixture__scorers")][1]//text()'))
 			if hgoals != "":
-				hgoals = f"{hicon}[⚽](#icon-ball) {hgoals}\n\n"
+				hgoals = f"{hicon}[âš½](#icon-ball) {hgoals}\n\n"
 			agoals = "".join(tree.xpath('.//ul[contains(@class,"fixture__scorers")][2]//text()'))
 			if agoals != "":
-				agoals = f"{aicon}[⚽](#icon-ball) {agoals}\n\n"
-			goals = f"{hgoals}{agoals}".replace(" minutes","")
+				agoals = f"{aicon}[âš½](#icon-ball) {agoals}\n\n"
+			self.goals = f"{hgoals}{agoals}".replace(" minutes","")
 			score = " - ".join(tree.xpath("//section[@class='fixture fixture--live-session-header']//span[@class='fixture__block']//text()")[0:2])
 			if score == "":
 				score = "v"
@@ -186,13 +194,13 @@ class MatchThreads:
 					infotime = "".join(i.xpath('.//span[2]/i/span/text()'))
 					infotime = infotime.replace('Booked at ','')
 					infotime = infotime.replace('mins','\'')
-					infos = infos.replace('sp-c-booking-card sp-c-booking-card--rotate sp-c-booking-card--yellow gs-u-ml','💛')
-					infos = infos.replace('booking-card booking-card--rotate booking-card--red gel-ml','🔴')
+					infos = infos.replace('sp-c-booking-card sp-c-booking-card--rotate sp-c-booking-card--yellow gs-u-ml','ðŸ’›')
+					infos = infos.replace('booking-card booking-card--rotate booking-card--red gel-ml','ðŸ”´')
 					subinfo = i.xpath('.//span[3]/span//text()')
 					subbed = subinfo[1] if subinfo else ""
 					subtime = subinfo[3].strip() if subinfo else ""
 					if subbed:
-						subbed = f"♻ {subbed} {subtime}"
+						subbed = f"â™» {subbed} {subtime}"
 					if infos:
 						if subbed:
 							thisplayer = f"**{player}** ({infos}{infotime}, {subbed})"
@@ -221,28 +229,27 @@ class MatchThreads:
 			awaysubs = [f"*{i}*" for i in subs[sublen:]]
 			awaysubs = ", ".join(awaysubs)
 				
-			statlookup = tree.xpath("//dl[@class='percentage-row']")
-			stats = f"\n{home}|v|{away}\n:--|:--:|--:\n"
+			statlookup = tree.xpath("//dl[contains(@class,'percentage-row')]")
+			self.stats = f"\n{home}|v|{away}\n:--|:--:|--:\n"
 			for i in statlookup:
 				stat = "".join(i.xpath('.//dt/text()'))
 				dd1 = "".join(i.xpath('.//dd[1]/span[2]/text()'))
 				dd2 = "".join(i.xpath('.//dd[2]/span[2]/text()'))
-				statline = f"{dd1} | {stat} | {dd2}\n"
-				stats += statline
-			stats += "\n"
-			dsc = f"[](#icon-discord)[Come join us on Discord](https://discord.gg/RtbyUQTV)\n\n\n\n"	
-			headerline = f"# {hicon} [{home}]({hreddit}) {score} [{away}]({areddit}) {aicon}\n\n"
+				self.stats += f"{dd1} | {stat} | {dd2}\n"
+			self.stats += "\n"
+			dsc = f"[](#icon-discord)[Come join us on Discord](https://discord.gg/tbyUQTV)\n\n\n\n"	
+			self.headerline = f"# {hicon} [{home}]({hreddit}) {score} [{away}]({areddit}) {aicon}\n\n"
 			if prematch == "Not found":
 				pass
 			else:
 				prematch = f"[Pre-Match Thread]({prematch})\n\n"
-			quickstats = f"{ko}{ground}{ref}{attenda}{prematch}{tv}{archive}"
-			quickstats += "[📻 Radio Commentary](https://www.nufc.co.uk/liveAudio.html)\n\n---\n\n"
-			lineups = f"{hicon} XI: {homexi}\n\nSubs: {homesubs}\n\n{aicon} XI: {awayxi}\n\nSubs: {awaysubs}\n\n"
+			self.quickstats = f"{ko}{ground}{ref}{attenda}{prematch}{tv}{archive}"
+			self.quickstats += "[ðŸ“» Radio Commentary](https://www.nufc.co.uk/liveAudio.html)\n\n---\n\n"
+			self.lineups = f"{hicon} XI: {homexi}\n\nSubs: {homesubs}\n\n{aicon} XI: {awayxi}\n\nSubs: {awaysubs}\n\n"
 			threadname = f"Match Thread: {home} v {away}"
 			bbcheader = f"##MATCH UPDATES (COURTESY OF [](#icon-bbc)[BBC]({link}))\n\n"
 
-			toptext = headerline+quickstats+lineups+goals+stats+dsc+bbcheader
+			toptext = self.headerline+self.quickstats+self.lineups+self.goals+self.stats+dsc+bbcheader
 			ticker = tree.xpath(".//div[@class='lx-stream__feed']/article")
 			return toptext,threadname,ticker
 		
@@ -251,7 +258,7 @@ class MatchThreads:
 		toptext,threadname,ticker = await update(prematch)
 
 		post = await self.bot.loop.run_in_executor(None,self.makepost,threadname,toptext)
-
+		self.matchurl = post.url
 		await modch.send(content=post.url)
 		tickids = []
 		ticker = ""
@@ -274,16 +281,16 @@ class MatchThreads:
 					content = content.replace(away,f"{aicon} {away}").strip()
 					if "Goal!" in header:
 						if "Own Goal" in content:
-							header = f"[⚽](#icon-OG) **OWN GOAL** "
+							header = f"[âš½](#icon-OG) **OWN GOAL** "
 						else:
-							header = f"[⚽](#icon-ball) **GOAL** "
+							header = f"[âš½](#icon-ball) **GOAL** "
 						time = f"**{time}**"
 						content = f"**{content.replace('Goal! ','').strip()}**"
 					if "Substitution" in header:
-						header = f"[🔄](#icon-sub) **SUB**"
+						header = f"[ðŸ”„](#icon-sub) **SUB**"
 						team,subs = content.replace("Substitution, ","").split('.',1)
 						on,off = subs.split('replaces')
-						content = f"**{team} [🔺](#icon-up){on} [🔻](#icon-down){off}**"
+						content = f"**{team} [ðŸ”º](#icon-up){on} [ðŸ”»](#icon-down){off}**"
 						time = f"**{time}**"
 					if content.lower().startswith("corner"):
 						content = f"[](#icon-corner) {content}"
@@ -296,7 +303,7 @@ class MatchThreads:
 							header = f"[OFF!](#icon-red) **RED**"
 						content = f"**{content}**"
 					if "injury" in content.lower() or "injured" in content.lower():
-						content = f"[🚑](#icon-injury) {content}"
+						content = f"[ðŸš‘](#icon-injury) {content}"
 					if "Full Time" in header:
 						stop = True
 						score = " - ".join(tree.xpath("//section[@class='fixture fixture--live-session-header']//span[@class='fixture__block']//text()")[0:2])
@@ -312,19 +319,15 @@ class MatchThreads:
 					upd = await self.bot.loop.run_in_executor(None,self.editpost,post,newcontent)
 					await modch.send("Match thread ended, submitting post-match thread.")
 					
-					print(f"Headerline: {headerline}")
-					print(f"matchurl: {post.url}")
-					print(f"quickstats: {quickstats}")
-					print(f"lineups: {lineups}")
-					print(f"goals: {goals}")
-					print(f"stats: {stats}")
+					print(f"goals: {self.goals}")
+					print(f"stats: {self.stats}")
 					print(f"home: {home}")
 					print(f"away: {away}")
 					print(f"score: {score}")
 					# Post post-match thread.
-					matchurl = f"[Match Thread]({post.url})\n\n"
+					matchurl = f"[Match Thread]({self.matchurl})\n\n"
 
-					posttext = headerline+quickstats+matchurl+lineups+goals+stats
+					posttext = self.headerline+self.quickstats+matchurl+self.lineups+self.goals+self.stats
 					threadname = f"Post-Match Thread: {home} {score} {away}"
 					post = await self.bot.loop.run_in_executor(None,self.makepost,threadname,posttext)
 					await modch.send(content=post.url)
